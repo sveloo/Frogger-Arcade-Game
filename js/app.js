d53ade4414c3d18cf511f314a28d84e2c0ec8feb
sveloo <sanjeev.veloo@gmail.com>
2016-08-09 01:42:16 +0800

    Added a couple more comments and cleaned up the formatting of the code.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 // This loads the Google font VT323 for use within Canvas
 WebFontConfig = {
-    google:{ families: ['VT323'] },
-    active: function(){start();},
+    google: {
+        families: ['VT323']
+    },
+    active: function() {
+        start();
+    },
 };
-(function(){
+(function() {
     var wf = document.createElement("script");
     wf.src = 'https://fonts.googleapis.com/css?family=VT323';
     wf.async = 'true';
@@ -12,12 +16,12 @@ WebFontConfig = {
 
 
 // Draw the scores
-var Score = function(){
+var Score = function() {
     ctx.clearRect(1, 590, 200, 50);
     ctx.font = "2.25em VT323";
     ctx.fillStyle = "yellow";
     ctx.textAlign = "left";
-    ctx.fillText("Score: "+ player.score, 1, 620);
+    ctx.fillText("Score: " + player.score, 1, 620);
 };
 
 
@@ -27,7 +31,7 @@ var Lives = function() {
     ctx.font = "2.25em VT323";
     ctx.fillStyle = "red";
     ctx.textAlign = "left";
-    ctx.fillText("Lives left: "+ player.lives, 310, 620);
+    ctx.fillText("Lives left: " + player.lives, 310, 620);
 }
 
 
@@ -50,15 +54,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed * dt;  // this ensures the game runs at the same speed across different computers
-    if (this.x > 505) {         // if the enemy reaches the end of the screen area,
-        this.x = 0;             // this resets its position to the start point on the x-axis
+    this.x += this.speed * dt; // this ensures the game runs at the same speed across different computers
+    if (this.x > 505) { // if the enemy reaches the end of the screen area,
+        this.x = 0; // this resets its position to the start point on the x-axis
     };
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  // Draws the enemy sprites
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); // Draws the enemy sprites
 };
 
 
@@ -67,22 +71,22 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 
 var Player = function(x, y) {
-    this.x = x;                             // sets the x-axis position of the player
-    this.y = y;                             // sets the y-axis position of the player
-    this.sprite = 'images/char-boy.png';    // assigs the image file, remember to note the pixel dimensions
-    this.score = 0;                         // sets the starting score to 0
-    this.lives = 3;                         // gives the player 3 lives
+    this.x = x; // sets the x-axis position of the player
+    this.y = y; // sets the y-axis position of the player
+    this.sprite = 'images/char-boy.png'; // assigs the image file, remember to note the pixel dimensions
+    this.score = 0; // sets the starting score to 0
+    this.lives = 3; // gives the player 3 lives
 };
 
 
 Player.prototype.update = function(dt) {
-    if (this.y <= -9) {                     // if the player reaches the end game y-axis point (-9), execute
-        player.gameWon();                   // game won function
+    if (this.y <= -9) { // if the player reaches the end game y-axis point (-9), execute
+        player.gameWon(); // game won function
     };
 };
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  // Draws the player sprite
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); // Draws the player sprite
     // Draws the header text, scores and lives
     ctx.clearRect(1, 1, 505, 35);
     ctx.font = "3em VT323";
@@ -143,14 +147,14 @@ document.addEventListener('keyup', function(e) {
 // following a tip from one of the Udacity instructors in the forums. It calculates the area of the PNG image (hence the need
 // for the height and width) and uses that to calculate the area of the image. If you change the PNG files, you must remember
 // to change the number values here too.
-var checkCollisions = function () {
+var checkCollisions = function() {
     for (var i = 0; i < 3; i++) {
-        if (player.x < allEnemies[i].x + 90 &&  // number value is the width of the enemy image in pixels
-            player.x + 67 > allEnemies[i].x &&  // number value is the width of the player image in pixels
-            player.y < allEnemies[i].y + 65 &&  // number value is the height of the enemy image in pixels
-            67 + player.y > allEnemies[i].y) {  // number value is the height of the enemy image in pixels
-        // collision detected!
-            player.gameLose();   // Reset the game if a collision is detected.
+        if (player.x < allEnemies[i].x + 90 && // number value is the width of the enemy image in pixels
+            player.x + 67 > allEnemies[i].x && // number value is the width of the player image in pixels
+            player.y < allEnemies[i].y + 65 && // number value is the height of the enemy image in pixels
+            67 + player.y > allEnemies[i].y) { // number value is the height of the enemy image in pixels
+            // collision detected!
+            player.gameLose(); // Reset the game if a collision is detected.
         };
     };
 };
@@ -179,5 +183,3 @@ Player.prototype.gameReset = function() {
     player.lives = 9;
     Score();
 };
-
-
